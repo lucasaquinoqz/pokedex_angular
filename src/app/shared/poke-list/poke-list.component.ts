3ae9a17aa443da90a561735579bedbff365f2f47
lucasaquinoqz <lucasaquinoq@gmail.com>
2023-11-23 23:40:45 -0300

    Adicionado a integração com a API

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
   selector: 'poke-list',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./poke-list.component.scss']
 })
 export class PokeListComponent {
+
+  public getAllPokemons: any
+  constructor(
+    private pokeApiService: PokeApiService 
+  ) { }
+
+  ngOnInit(): void{
+    this.pokeApiService.apiListAllPokemons.subscribe(
+      res => {
+        this.getAllPokemons = res.results
+        console.log(this.getAllPokemons)
+      }
+    )
+  }
 
 }
